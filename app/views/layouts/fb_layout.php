df8fe556c634f06a21c2f7204a92605b0969ab08
Ayrton Gomes <com.ayrton@gmail.com>
2017-06-14 03:20:55 -0100

    still working on stuff

---
+++ app/views/layouts/fb_layout.php
@@ -0,0 +1,59 @@
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en">
+<head>
+    <title>Trender Platform!</title>
+    <meta name="referrer" content="origin-when-crossorigin" id="meta_referrer">
+    <link rel="stylesheet" href="static/css/fb.main.css"  />
+    <style type="text/css">
+        .good-syrene {
+            padding: 3px;
+            background-color: greenyellow;
+            display: inline-block;
+            border-radius: 15px;
+        }
+
+        .alarm-syrene {
+            padding: 3.5px;
+            background-color: tomato;
+            display: inline-block;
+            border-radius: 15px;
+        }
+
+        .di {
+            padding: 12px;
+            margin: 0px;
+            border-style: none;
+            border-bottom: 1px solid #eee;
+        }
+
+        .d #viewport {
+            margin: 0px;
+            margin-left: 20px;
+            max-width: initial;
+            display: inline-flex;
+        }
+        
+        div#objects_container {
+            width: 600px;
+            display: inline-block;
+        }
+        div.brand-picture-instance {
+            display:inline;
+        }
+        td span.alarm-syrene {
+            margin-top: -19px;
+            margin-left: -9px;
+        }
+        div#sidebar {
+            width: 400px;
+            margin-left: 20px;
+            background-color: #fff;
+            padding: 10px;
+            height: 500px;
+        }
+        /*
+#lightblue
+#tomato
+#orangered
+        */
+    </style>
+</head>
