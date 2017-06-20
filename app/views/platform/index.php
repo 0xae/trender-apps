ab710c74b4f986d2094d89c7d878fbd149735473
Ayrton Gomes <com.ayrton@gmail.com>
2017-06-20 22:39:38 -0100

    welcome div

--- app/views/platform/index.php
+++ app/views/platform/index.php
@@ -4,12 +4,29 @@ $this->title = 'Trender Platform';
  
 <div class="row">
     <div class="col-md-2" style="padding:10px;padding-top: 50px;padding-left: 20px;height:100%;position:absolute; box-shadow: -1px 0 0 #392e5c inset;background-color: #2c2541;">
-        <div class="" style="margin-top: 20px;">
+        <div style="margin-top: 20px;">
+            <h5 class="title">
+                Welcome to Trender
+            </h5>
+        </div>
+
+        <div class="" style="margin-left: -12px;margin-bottom: 50px;">
+            <img src="static/img/connected.png" />
+            <center>
+            <h5 style="color: #cbc8d0;margin-left:12px;"> 
+                <small>
+                Trender crawls the internet and feed you relevant
+                and rich information.
+                </small>
+            </h5>
+            </center>
+        </div>
+
+        <div style="margin-top: 20px;">
             <h5 class="title">
                 <strong><span style="color: #ffb300; font-size: 12px; margin-right: 5px;" class="fa fa-line-chart"></span></strong>  
                 Trending topics
             </h5>
-
         </div>
 
         <div style="margin-left: 15px;">
@@ -45,7 +62,30 @@ $this->title = 'Trender Platform';
                     </span>
                 </small>
             </h5>
+
+            <h5 style="color: #cbc8d0"> 
+                #WhiteHouseEaster <br/>
+                <small>
+                    2hrs ago
+                    <span style="padding-left: 10px;">
+                      <div class="live-indicator-away"></div>
+                        210 posts
+                    </span>
+                </small>
+            </h5>
+
+            <h5 style="color: #cbc8d0"> 
+                #WhiteHouseEaster <br/>
+                <small>
+                    2hrs ago
+                    <span style="padding-left: 10px;">
+                      <div class="live-indicator-away"></div>
+                        210 posts
+                    </span>
+                </small>
+            </h5>
         </div>
+
     </div>
 
     <div class="col-md-8" style="margin-left:250px;margin-top: 50px;">
