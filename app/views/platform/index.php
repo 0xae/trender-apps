4976bb8164742c270fe317beef969c754d3e5dc0
Ayrton Gomes <com.ayrton@gmail.com>
2017-06-20 22:13:41 -0100

    working on trending-topics design

--- app/views/platform/index.php
+++ app/views/platform/index.php
@@ -9,6 +9,42 @@ $this->title = 'Trender Platform';
                 <strong><span style="color: #ffb300; font-size: 12px; margin-right: 5px;" class="fa fa-line-chart"></span></strong>  
                 Trending topics
             </h5>
+
+        </div>
+
+        <div style="margin-left: 15px;">
+            <h5 style="color: #cbc8d0"> 
+                Daniel Day-Lewis <br/>
+                <small>
+                    2mn ago
+                    <span style="padding-left: 10px;">
+                      <div class="live-indicator"></div>
+                        21,834 posts
+                    </span>
+                </small>
+            </h5>
+
+            <h5 style="color: #cbc8d0"> 
+                #FirstGifComesUp <br/>
+                <small>
+                    12mn ago
+                    <span style="padding-left: 10px;">
+                      <div class="live-indicator"></div>
+                        8721 posts
+                    </span>
+                </small>
+            </h5>
+
+            <h5 style="color: #cbc8d0"> 
+                #WhiteHouseEaster <br/>
+                <small>
+                    2hrs ago
+                    <span style="padding-left: 10px;">
+                      <div class="live-indicator-away"></div>
+                        210 posts
+                    </span>
+                </small>
+            </h5>
         </div>
     </div>
 
