d86984f5ac56048f596d9b4678e390becba6d7dc
Ayrton Gomes <com.ayrton@gmail.com>
2017-06-20 21:41:27 -0100

    working on stuff. yeee

--- app/views/layouts/bootstrap_layout.php
+++ app/views/layouts/bootstrap_layout.php
@@ -57,13 +57,7 @@ AppAsset::register($this);
             </div>
         </div>
     </nav>
-    <div class="row" style="padding:0px;">
-        <div class="col-md-2" style="padding:0px;height:100%;position:absolute; box-shadow: -1px 0 0 #392e5c inset;   background-color: #2c2541;">
-        </div>
-        <div class="col-md-8" style="margin-left:250px;margin-top: 50px;">
-            <?= $content ?>
-        </div>
-    </div>
+    <div style="padding:0px;"> <?= $content ?> </div>
 </div>
 
 <!--
