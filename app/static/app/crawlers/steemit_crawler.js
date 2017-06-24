6e4d60634ea80032d93170a6812d4481cbf9731b
Ayrton Gomes <com.ayrton@gmail.com>
2017-06-24 00:03:37 -0100

    working on crawlers

--- app/static/app/crawlers/steemit_crawler.js
+++ app/static/app/crawlers/steemit_crawler.js
@@ -128,40 +128,6 @@
         });
     }
 
-    var profile = null;
-    function CrawlPictures() {
-        if (profile) {
-            var st = document.getElementsByClassName("Userpic")[1].attributes.style;
-            var picUrl = (/url\((.*)\)/.exec(st.value)[1]).replace(/"/g,'');
-            var userId = profile.id;
-            profile = null;
-
-            fetch('http://127.0.0.1:5000/profile/'+userId+'/picture', {
-                body:picUrl,
-                method: 'POST',
-                headers: {'Content-Type':'application/json'}
-            }).then(function () {
-                console.log("Update done...");
-            });
-        } else {
-            fetch('http://127.0.0.1:5000/profile/nopicture', {method:'GET', header:{'Content-Type':'application/json'}})
-            .then(function (response) {
-                response.json()
-                .then(function (data){
-                if (!data.length) return;
-                    profile = data.pop();
-                });
-            });
-        }
-    }
-
-    function getNoPictureProfiles() {
-        return fetch('http://127.0.0.1:5000/profile/nopicture')
-        .then(function (response) {
-          return response.json();
-        });
-    }
-
     function _foreach(ary, callback) {
         for (var i=0; i<ary.length; i++) {
             var el = ary[i];
