cf9b4e584933a68d7b4d15f010edb6b349df6dfc
Ayrton Gomes <com.ayrton@gmail.com>
2017-07-05 01:00:13 -0100

    working on stuff

---
+++ app/views/platform/helpers/sidebar.php
@@ -0,0 +1,81 @@
+        <div style="margin-top: 20px;">
+            <h5 class="title">
+                Welcome to Trender
+            </h5>
+        </div>
+
+        <div class="" style="margin-left: -12px;margin-bottom: 50px;">
+            <img src="static/img/connected.png" />
+            <center>
+            <h5 style="color: #cbc8d0;margin-left:12px;"> 
+                <small>
+                Trender crawls the internet and feed you relevant
+                and rich information.
+                </small>
+            </h5>
+            </center>
+        </div>
+
+        <div style="margin-top: 20px;">
+            <h5 class="title">
+                <strong><span style="color: #ffb300; font-size: 12px; margin-right: 5px;" class="fa fa-line-chart"></span></strong>  
+                Trending topics
+            </h5>
+        </div>
+
+        <div style="margin-left: 15px;">
+            <h5 style="color: #cbc8d0"> 
+                Daniel Day-Lewis <br/>
+                <small>
+                    2mn ago
+                    <span style="padding-left: 10px;">
+                      <div class="live-indicator"></div>
+                        21,834 posts
+                    </span>
+                </small>
+            </h5>
+
+            <h5 style="color: #cbc8d0"> 
+                #FirstGifComesUp <br/>
+                <small>
+                    12mn ago
+                    <span style="padding-left: 10px;">
+                      <div class="live-indicator"></div>
+                        8721 posts
+                    </span>
+                </small>
+            </h5>
+
+            <h5 style="color: #cbc8d0"> 
+                #WhiteHouseEaster <br/>
+                <small>
+                    2hrs ago
+                    <span style="padding-left: 10px;">
+                      <div class="live-indicator-away"></div>
+                        210 posts
+                    </span>
+                </small>
+            </h5>
+
+            <h5 style="color: #cbc8d0"> 
+                #WhiteHouseEaster <br/>
+                <small>
+                    2hrs ago
+                    <span style="padding-left: 10px;">
+                      <div class="live-indicator-away"></div>
+                        210 posts
+                    </span>
+                </small>
+            </h5>
+
+            <h5 style="color: #cbc8d0"> 
+                #WhiteHouseEaster <br/>
+                <small>
+                    2hrs ago
+                    <span style="padding-left: 10px;">
+                      <div class="live-indicator-away"></div>
+                        210 posts
+                    </span>
+                </small>
+            </h5>
+        </div>
