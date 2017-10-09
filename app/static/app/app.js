2e4fc45e776a30417f1abc32b667fe4f90b9187a
Ayrton Gomes <com.ayrton@gmail.com>
2017-10-09 10:16:34 -0100

    working on the module system

--- app/static/app/app.js
+++ app/static/app/app.js
@@ -1,21 +1,13 @@
-/*
-angular.module('trender', [])
-.factory('app', function () {
+define("trender/app", function () {
     function promisify(prms) {
         return new Promise(function (resolve, reject){
             prms.then(resolve, reject);
         });
     }
 
-});
-*/
-
-define("trender/app", [], function () {
     var server = {
         api: 'http://127.0.0.1:5000/api/'
     };
-    
-    console.info("hello ");
 
     return {
         server: server
@@ -26,7 +18,7 @@ requirejs(['jquery', '_', 'trender/app'], function ($, _, app) {
     _.templateSettings = {
         interpolate: /\{\{(.+?)\}\}/g
     };
-    
+
     console.info("app server is at", app);
 });
 
